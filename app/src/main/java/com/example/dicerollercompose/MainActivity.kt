b7c7889d66d156b22c76c2bdd11c2cace2b74e01
Agus Adi <gusadi872@gmail.com>
2022-10-23 19:31:50 +0700

    complete the dice roller logic

--- app/src/main/java/com/example/dicerollercompose/MainActivity.kt
+++ app/src/main/java/com/example/dicerollercompose/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -37,15 +37,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result: Int = 1
+    var result by remember {
+        mutableStateOf(1)
+    }
+
+    val diceImage = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
 
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResource(id = R.drawable.dice_1),
-            contentDescription = "Dice number 1"
+            painter = painterResource(id = diceImage),
+            contentDescription = "Dice number $result"
         )
         
         Spacer(modifier = Modifier.height(16.dp))
